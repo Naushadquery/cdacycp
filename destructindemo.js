48972177b5c0fd3fa5f29835b6034c700132d2d0
Naushad Akhtar <110463188+Naushadquery@users.noreply.github.com>
2024-12-31 16:43:37 +0530

    Add files via upload

---
+++ destructindemo.js
@@ -0,0 +1,29 @@
+// let arr = [2, 5, 7];
+// let [a, b, c] = arr;
+// console.log(a, b);
+// console.log("------------------1----------------------");
+// let num = [1, 3, 5, 6, 5, 9, 7];
+// let [a1, b1, ...rest] = num;
+// console.log(a1, b1);
+// rest.forEach((val) => console.log(val));
+//  console.log("------------------2----------------------");
+// let score = [1, 3, 5, 6, 5, 9, 7];
+// let [a2, , , ...rest2] = score;
+// console.log(a2, ...rest2);
+// console.log(...rest2);
+// console.log("------------------3----------------------");
+let emp = { id: 1, name: "naushad" };
+let { id, name } = emp;
+console.log(name);
+// console.log("------------------4----------------------");
+// function sum(n1, n2, n3) {
+//   return n1 + n2 + n3;
+// }
+// ans = sum(...num);
+// console.log(ans);
+// console.log("-------------------4---------------------");
+// let obj = { id: 1, name: "akhtar", city: "mumbai" };
+// let updatedObj = { ...obj, city: "pune" };
+// console.log(updatedObj);
+// console.log(obj);
+// console.log("----------------------------------------");
